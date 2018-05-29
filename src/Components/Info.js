293398ceb37a1e6cc431dae6346d9a85750ad58b
Raymond Kim <kimraymond9@gmail.com>
2018-05-30 01:28:13 +1200

    input username and champion to get data

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -6,37 +6,27 @@ import { getUserData } from '../actions.js';
 class Info extends React.Component {
 
   render() {
-<<<<<<< HEAD
     const match = this.props.match;
     const input = this.props.input;
-    if(match.length < 5){
+    if (match.length < 5) {
       return null;
     }
     var i;
-    for(i = 0; i < match.length; i ++){
+    for (i = 0; i < match.length; i++) {
       this.props.dispatch(getUserData(match[i], input));
     }
-      return (
-        <div>
-          <p>Game ID: {this.props.match[0].gameId}</p>
-          <p>Game ID: {this.props.match[1].gameId}</p>
-          <p>Game ID: {this.props.match[2].gameId}</p>
-          <p>Game ID: {this.props.match[3].gameId}</p>
-          <p>Game ID: {this.props.match[4].gameId}</p>
-        </div>
-      );
-=======
     return (
-            <div>
-      <p>
-          Username: {this.props.match[0].gameId}
-      </p>
+      <div>
+        <p>Game ID: {this.props.match[0].gameId}</p>
+        <p>Game ID: {this.props.match[1].gameId}</p>
+        <p>Game ID: {this.props.match[2].gameId}</p>
+        <p>Game ID: {this.props.match[3].gameId}</p>
+        <p>Game ID: {this.props.match[4].gameId}</p>
       </div>
     );
->>>>>>> bb5f5aabbe04da3aaff04057a980e23bf793fa75
   }
 }
 
- const mapStateToProps = ({ match, input }) => ({ match, input });
+const mapStateToProps = ({ match, input }) => ({ match, input });
 
 export default connect(mapStateToProps)(Info);
